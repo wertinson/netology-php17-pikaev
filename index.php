c505ece11682fc74bb28887d0e85e956c263d7bc
wertinson <33161892+wertinson@users.noreply.github.com>
2017-10-30 18:58:53 +0400

    Second!

--- index.php
+++ index.php
@@ -6,28 +6,28 @@ $email = "mymail@hotmail.com";
 $city = "Самара";
 $caption = "Большие амбиции для маленьких учреждений";
 
-echo "<h1>Страница пользователя $name</h1>";
-echo "<table>";
-  echo "<tr>";
-    echo "<td><p>Имя</p></td>";
-    echo "<td><p>$name</p></td>";
-  echo "</tr>";
-  echo "<tr>";
-    echo "<td><p>Возраст</p></td>";
-    echo "<td><p>$age</p></td>";
-  echo "</tr>";
-  echo "<tr>";
-    echo "<td><p>Адрес электронной почты</p></td>";
-    echo "<td><a href='mailto:$email'>$email</a></td>";
-  echo "</tr>";
-  echo "<tr>";
-    echo "<td><p>Город</p></td>";
-    echo "<td><p>$city</p></td>";
-  echo "</tr>";
-  echo "<tr>";
-    echo "<td><p>О себе</p></td>";
-    echo "<td><p>$caption</p></td>";
-  echo "</tr>";
-echo "</table>";
-
 ?>
+
+<h1>Страница пользователя <?php echo $name ?></h1>
+<table>
+  <tr>
+    <td><p>Имя</p></td>
+    <td><p><?php echo $name ?></p></td>
+  </tr>
+  <tr>
+    <td><p>Возраст</p></td>
+    <td><p><?php echo $age ?></p></td>
+  </tr>
+  <tr>
+    <td><p>Адрес электронной почты</p></td>
+    <td><a href='mailto:$email'><?php echo $email ?></a></td>
+  </tr>
+  <tr>
+    <td><p>Город</p></td>
+    <td><p><?php echo $city ?></p></td>
+  </tr>
+  <tr>
+    <td><p>О себе</p></td>
+    <td><p><?php echo $caption ?></p></td>
+  </tr>
+</table>
